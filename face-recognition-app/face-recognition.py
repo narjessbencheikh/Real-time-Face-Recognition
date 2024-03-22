9f2c722f3e591a9bd54948676cef6b41df07a17a
narjessbencheikh <47827834+narjessbencheikh@users.noreply.github.com>
2024-03-22 14:05:33 +0100

    Add files via upload

--- face-recognition-app/face-recognition.py
+++ face-recognition-app/face-recognition.py
@@ -1,20 +1,28 @@
 import cv2
 from simple_facerec import SimpleFacerec
 
-#Encode Faces from a fold
+#Encode Images from a folder
 sf = SimpleFacerec()
 sf.load_encoding_images("images/")
 
-#Load Camera
+#Face Recognition In Real-Time On A Webcam
+
+ #Load Camera 
 cap = cv2.VideoCapture(0)
 
+#Take Webcam Stream
 while True :
   ret, frame = cap.read()
 
+#Face Location And Face Recognition
+  
   #Detect Faces 
   face_locations, face_names = sf.detect_known_faces(frame)
   for face_loc, name in zip(face_locations, face_names):
+    # (y1, x2, y2, x1)
+    #y1 : the upper-left corner, x2 : the upper-right corner, y2 : the lower-right corner, x1 : the lower-left corner of the bounding box
     y1, x2, y2, x1 = face_loc[0], face_loc[1], face_loc[2], face_loc[3]
+
     cv2.putText(frame, name, (x1, y1-10), cv2.FONT_HERSHEY_DUPLEX, 1, (0,0,200), 2)
     cv2.rectangle(frame, (x1, y1), (x2, y2), (0,0,200), 2)
   
--- face-recognition-app/image_comparison.py
+++ face-recognition-app/image_comparison.py
@@ -1,14 +1,20 @@
+#Importing opencv and face_recognition libraries
 import cv2
 import face_recognition
 
-img = cv2.imread("Cristiano1.jpg")
-rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-img_encoding = face_recognition.face_encodings(rgb_img)[0]
+#Face Encoding First Image
+img = cv2.imread("Cristiano1.jpg") #load the image 
+#we need to convert it into RGB color format because OpenCV, by default, reads images in BGR format, 
+#while the face_recognition library expects images in RGB format.
+rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) 
+img_encoding = face_recognition.face_encodings(rgb_img)[0] #face encoding with the functions of the Face recognition library
 
-img2 = cv2.imread("images/Cristiano.jpg")
+#Face Encoding Second Image
+img2 = cv2.imread("images/Cristiano.webp") 
 rgb_img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)
 img_encoding2 = face_recognition.face_encodings(rgb_img2)[0]
 
+#Comparison Of the 2 images after encoding
 result = face_recognition.compare_faces([img_encoding], img_encoding2)
 print("Result: ", result)
 
